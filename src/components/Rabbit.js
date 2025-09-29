38dbae8872afeb47fa4897fb1829efec1a5456ff
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-09-28 22:49:44 -0500

    create images and show rabbit on screen

---
+++ src/components/Rabbit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/rabbit-sit.png'
+
+const Rabbit = () => {
+  return (
+    <div>
+      <img src={require('../assets/rabbit-sit.png')} alt="Rabbit Sitting" />
+    </div>
+  )
+}
+
+export default Rabbit
