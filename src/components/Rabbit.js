8bf14d8bbe15f8ce2bbd9496b07da28250d4f8f8
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-10-02 22:30:02 -0500

    added context for clicking and spawning a rabbit where the mouse cursor is

--- src/components/Rabbit.js
+++ src/components/Rabbit.js
@@ -4,7 +4,7 @@ import rabbitSit from '../assets/rabbit-sit.png'
 const Rabbit = (props) => {
   return (
     <div>
-      <img src={rabbitSit} style={{ position: 'absolute', left: props.x, top: props.y, height:60, width:60, zIndex:-1 }} alt="Rabbit Sitting" />
+      <img src={rabbitSit} style={{ position: 'absolute', left: props.x, top: props.y, height:60, width:60}} alt="Rabbit Sitting" />
     </div>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,32 +2,71 @@ import React, { useEffect, useState } from 'react'
 import Rabbit from '../components/Rabbit';
 
 const HomePage = () => {
-  const spawnRabbits = () => {
-    //fetch rabbit data from backend
-    setRabbits(rabbits => [...rabbits, {id: rabbits.length+1, xaxis: Math.floor(Math.random() * (window.innerWidth - 60)), yaxis: Math.floor(Math.random() * (window.innerHeight - 60))}]);
+  const [rabbitSize, setRabbitSize] = useState(60);
+  const [rabbits, setRabbits] = useState([])
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setMousePosition({
+      x: event.clientX, // or event.pageX, event.screenX
+      y: event.clientY, // or event.pageY, event.screenY
+    });
   };
-  const [rabbits, setRabbits] = useState([]);
+  const handleClick = (event, id) => {
+
+    if (event.type === 'click') {
+      console.log('Left click');
+      spawnRabbits();
+    } else if (event.type === 'contextmenu') {
+      console.log('Right click');
+      deSpawnSelectedRabbit(id);
+    }
+  }
+  const spawnRabbits = () => {
+    
+    //fetch rabbit data from backend
+    setRabbits(rabbits => [...rabbits, {id: rabbits.length+1, xaxis: mousePosition.x-rabbitSize/2, yaxis: mousePosition.y-rabbitSize/2}]);
+    // setRabbits(rabbits => [...rabbits, {id: rabbits.length+1, xaxis: Math.floor(Math.random() * (window.innerWidth - rabbitSize)), yaxis: Math.floor(Math.random() * (window.innerHeight - rabbitSize))}]);
+  };
+  ;
+
+  const deSpawnLastRabbit = () => {
+    //fetch rabbit data from backend
+    const tempRabbits = rabbits.slice(0, -1); 
+    setRabbits(tempRabbits);
+  };
+  const deSpawnSelectedRabbit = (id) => {
+    //fetch rabbit data from backend
+    //const tempRabbits = rabbits.slice(0, -1); 
+    //setRabbits(tempRabbits);
+    setRabbits(prevRabbits => prevRabbits.filter(rabbit => rabbit.id !== id));
+    console.log(id)
+  };
+  
 
   useEffect(() => {
-    setRabbits([
-    {id: 1, xaxis: Math.floor(Math.random() * (window.innerWidth - 60)), yaxis: Math.floor(Math.random() * (window.innerHeight - 60))},
-    {id: 2, xaxis: Math.floor(Math.random() * (window.innerWidth - 60)), yaxis: Math.floor(Math.random() * (window.innerHeight - 60))},
-    {id: 3, xaxis: Math.floor(Math.random() * (window.innerWidth - 60)), yaxis: Math.floor(Math.random() * (window.innerHeight - 60))}
-    ]);
+    window.addEventListener('mousemove', handleMouseMove);
+    // setRabbits([
+    // // {id: 1, xaxis: Math.floor(Math.random() * (window.innerWidth - rabbitSize)), yaxis: Math.floor(Math.random() * (window.innerHeight - rabbitSize))},
+    // // {id: 2, xaxis: Math.floor(Math.random() * (window.innerWidth - rabbitSize)), yaxis: Math.floor(Math.random() * (window.innerHeight - rabbitSize))},
+    // // {id: 3, xaxis: Math.floor(Math.random() * (window.innerWidth - rabbitSize)), yaxis: Math.floor(Math.random() * (window.innerHeight - rabbitSize))}
+    // ]);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
   }, []);
 
   return (
-    <div>
+    <div onClick={handleClick} style={{height: '100vh', width: '100vw', overflow: 'hidden', position: 'relative'}}>
       <h1>Home Page</h1>
-      <button onClick={spawnRabbits}>Get Rabbits</button>
-      {console.log(rabbits)}
+      <button onClick={(e) => { e.stopPropagation(); deSpawnLastRabbit(); }} style={{margin:'20px'}}>Despawn Last Rabbit</button>
       {rabbits.map(rabbit => (
-        <div key={rabbit.id}> 
-          <Rabbit x={rabbit.xaxis} y={rabbit.yaxis}/>
-        </div>
+        
+          <Rabbit key={rabbit.id} x={rabbit.xaxis} y={rabbit.yaxis} onContextMenu={(e) => {
+      e.preventDefault(); // prevent browser context menu
+      deSpawnSelectedRabbit(rabbit.id);}}/>
       ))}
     </div>
-    
   )
 }
 
