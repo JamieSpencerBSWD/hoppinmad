01b20d8fda8b22122e189c03d78b8a6cb47538d7
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-10-18 22:05:38 -0500

    Update Rabbit.js

--- src/components/Rabbit.js
+++ src/components/Rabbit.js
@@ -4,7 +4,13 @@ import rabbitSit from '../assets/rabbit-sit.png'
 const Rabbit = (props) => {
   return (
     <div>
-      <img className='move-down' onContextMenu={(e) => {e.preventDefault(); e.stopPropagation(); props.onRemove(props.id)}} src={rabbitSit} style={{ position: 'absolute', left: props.x, top: props.y, rotate:"45deg", height:60, width:60, zIndex:0 }} alt="Rabbit Sitting" />
+      <img 
+        
+        onContextMenu={(e) => {e.preventDefault(); e.stopPropagation(); props.onRemove(props.id)}} 
+        src={rabbitSit} 
+        style={{ position: 'absolute', left: props.x, top: props.y, height:60, width:60, zIndex:0 }} 
+        alt="Rabbit Sitting" 
+      />
     </div>
   )
 }
