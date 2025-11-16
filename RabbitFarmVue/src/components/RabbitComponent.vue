256f8bec16c5207ad12a82a10ba05209fd189757
DakotaSpencer <dakota.spencer0327@gmail.com>
2025-11-15 23:01:39 -0600

    Update gravity and begin implementation for dragging. Will update position tomorrow

--- RabbitFarmVue/src/components/RabbitComponent.vue
+++ RabbitFarmVue/src/components/RabbitComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-defineProps({
+import { ref, onMounted, watch } from 'vue'
+const props = defineProps({
   id: {
     type: Number,
     required: true,
@@ -17,66 +17,109 @@ defineProps({
     type: Number,
     required: true,
   },
-});
+  dragged: {
+    type: Boolean,
+    required: true,
+  },
+  imgWidth: {
+    type: Number,
+    required: true,
+  },
+  imgHeight: {
+    type: Number,
+    required: true,
+  },
+})
 
 const rabbitRef = ref(null)
-const fallSpeed = ref(10); // how fast we fall
+const fallSpeed = ref(2) // how fast we fall
 const distance = ref(0)
-const rabbitState = ref("")
+const rabbitState = ref('')
+const rabbitIMG = ref('')
 
-//Make Rabbit Interactable, and able to be dragged. Keep track of STATE (dragged, falling, jumping, sleeping, etc) in a ref attribute, 
+const posX = ref()
+const posY = ref()
+
+watch(
+  () => props.dragged,
+  (newValue) => {
+    //When mouse is Down/dragged = true, set Fall Speed to 0, so no gravity is applied
+    //if dragged is False, set speed back to 8
+    rabbitState.value = 'DRAGGING'
+    newValue == true ? (fallSpeed.value = 0) : (fallSpeed.value = 2)
+    //update Rabbit Position to be where the mouse is, at all times
+    moveRabbit()
+  },
+)
+//Make Rabbit Interactable, and able to be dragged. Keep track of STATE (dragged, falling, jumping, sleeping, etc) in a ref attribute,
 // and update the bottom to say what state is
-
-
+const moveRabbit = () => {
+  console.log(props.positionX, props.positionY)
+  //Handle Logic for moving rabbit around / having rabbit follow mouse in here
+}
 onMounted(() => {
   const gravity = () => {
-    const element   = rabbitRef.value;
+    const element = rabbitRef.value
     const tick = () => {
       //get parent Div
-      const container = element.offsetParent;
+      const container = element.offsetParent
       //if no parent div, return
       if (!container) {
-        return;
+        return
       }
-      const containerRect = container.getBoundingClientRect();
-      const containerBottom = containerRect.bottom;
       // get the height of the window
-      //const { innerHeight } = window;
+      const containerRect = container.getBoundingClientRect()
+      const containerBottom = containerRect.bottom
 
       // get the position of the bottom of the element on the screen
-      const { bottom } = element.getBoundingClientRect();
-
+      const { bottom } = element.getBoundingClientRect()
       // if we're not at the bottom of the screen...
-      if(bottom < containerBottom){
+      if (bottom < containerBottom) {
         //Update Animation Frame or PNG to show Falling State
-          rabbitState.value="FALLING"
-          // ...increase the distance we've fallen...
-          distance.value += fallSpeed.value;
+        if (props.dragged != true) {
+          rabbitState.value = 'FALLING'
+        }
+        // ...increase the distance we've fallen...
+        distance.value += fallSpeed.value
+        // ...apply the fallen distance to the element...
+        element.style.transform = `translateY(${distance.value}px)`
+        // Update the Position X and Y properties in the component
 
-          // ...apply the fallen distance to the element...
-          element.style.transform = `translateY(${distance.value}px)`;
-
-          // ...and queue up the next frame to do it all again
-          requestAnimationFrame(tick);
-      }else{
-        rabbitState.value="IDLE"
+        //these two just keep track of the current position of the component, so that we may see it
+        posX.value = element.getBoundingClientRect().left
+        posY.value = element.getBoundingClientRect().top
+        // ...and queue up the next frame to do it all again
+        requestAnimationFrame(tick)
+      } else {
+        rabbitState.value = 'IDLE'
       }
+      rabbitIMG.value = '/rabbit' + rabbitState.value + '.png'
     }
-    requestAnimationFrame(tick);
+    requestAnimationFrame(tick)
   }
   gravity()
 })
-
 //handle state for rabbit here. If rabbit is interacted with,
 //  pass whatever is interacted with through properties
 </script>
 
 <template>
-  <div class="rabbit" ref="rabbitRef">
-    <div>
-      <img src="../../public/rabbit-sit.png" style="height: 60px; width: 60px" />
+  <div
+    class="rabbit"
+    ref="rabbitRef"
+    :style="{
+      left: props.positionX + 'px',
+      top: props.positionY + 'px',
+      // left: props.positionX  - props.imgWidth / 2 + 'px',
+      // top: props.positionY  - props.imgHeight / 2 + 'px',
+    }"
+  >
+    <div style="height: 60px; width: 60px; background-color: green;">
+      <!-- Change so Style (rotate 45 deg) is applied when in FALLING state, and rotate(0deg) is applied when in IDLE state -->
+      <!-- <img src="/rabbit-sit.png" style="height: 60px; width: 60px" /> -->
     </div>
-    <div>{{ rabbitState }}</div>
+    <div>({{ dragged }}) - {{ rabbitState }}</div>
+    <p>{{ posX }}, {{ posY }}</p>
   </div>
 </template>
 
--- RabbitFarmVue/src/views/HomePage.vue
+++ RabbitFarmVue/src/views/HomePage.vue
@@ -1,119 +1,146 @@
 <script setup>
-import { ref, reactive } from 'vue';
-import Rabbit from '../components/RabbitComponent.vue';
+import { ref, reactive } from 'vue'
+import Rabbit from '../components/RabbitComponent.vue'
 
-let retrievedStorage = localStorage.getItem('rabbitsLS');
-let retArray = JSON.parse(retrievedStorage);
-console.log(retArray);
+let retrievedStorage = localStorage.getItem('rabbitsLS')
+let retArray = JSON.parse(retrievedStorage)
 
-const rabbitID = ref(retArray ? retArray.length : 0);
-const rabbitArray = ref(retArray ? retArray : []);
+const rabbitID = ref(retArray ? retArray.length : 0)
+const rabbitArray = ref(retArray ? retArray : [])
 
+const dragging = ref(false)
+//Save Mouse Position
+const mousePosition = reactive({
+  x: 0,
+  y: 0,
+})
+const hover = ref(false)
+//creates a new rabbit at the current mouse position and persists it in localStorage
+    //Also uses rabbit Schema
 const addRabbit = () => {
-    const id = rabbitID.value++; //Get Rabbit ID
-    const { x, y } = mousePosition; //get Mouse Position X and Y
+  const id = rabbitID.value++ //Get Rabbit ID
+  const { x, y } = mousePosition //get Mouse Position X and Y
 
-    rabbitArray.value.push({
-        id,
-        name: `Rabbit ${id}`,
-        positionX: x,
-        positionY: y,
-        imgWidth: 60,
-        imgHeight: 60,
-    }); //Push to Array, following Schema in Rabbit Component
+  rabbitArray.value.push({
+    id,
+    name: `Rabbit ${id}`,
+    positionX: x,
+    positionY: y,
+    dragged: false,
+    imgWidth: 60,
+    imgHeight: 60,
+  }) //Push to Array, following Schema in Rabbit Component
 
-    let rabbits = JSON.stringify(rabbitArray.value);
-    localStorage.setItem('rabbitsLS', rabbits);
-};
-const removeRabbit = (id) => {
-  if (id == null) {
-    rabbitArray.value.pop();
-  } else {
-    const index = rabbitArray.value.findIndex(r => r.id === id);
-
-    if (index !== -1) {
-      console.log("Removing rabbit with ID:", id);
-      rabbitArray.value.splice(index, 1);
-    }
-  }
-  localStorage.setItem('rabbitsLS', JSON.stringify(rabbitArray.value));
-};
-
-const clearAllRabbits = () => {
-    rabbitArray.value = []
-    rabbitID.value = 0
-    localStorage.clear()
+  let rabbits = JSON.stringify(rabbitArray.value)
+  localStorage.setItem('rabbitsLS', rabbits)
 }
 
-//Demo code to get Mouse Position
-const mousePosition = reactive({
-    x: 0,
-    y: 0,
-});
-const hover = ref(false);
+//removes rabbit that was right-clicked, and updates local storage
+    //if id is not provided (i.e., just clicking on the FIELD div), deletes the lat rabbit in the array
+const removeRabbit = (id) => {
+  if (id == null) {
+    rabbitArray.value.pop()
+  } else {
+    const index = rabbitArray.value.findIndex((r) => r.id === id)
 
+    if (index !== -1) {
+      console.log('Removing rabbit with ID:', id)
+      rabbitArray.value.splice(index, 1)
+    }
+  }
+  localStorage.setItem('rabbitsLS', JSON.stringify(rabbitArray.value))
+}
+
+// Clears all rabbits, resets the array and increment value, and clears local storage
+const clearAllRabbits = () => {
+  rabbitArray.value = []
+  rabbitID.value = 0
+  localStorage.clear()
+}
+
+
+
+//Handles Mouse Position
 const handleMouseMove = (event) => {
-    hover.value = true;
-    mousePosition.x = event.clientX;
-    mousePosition.y = event.clientY;
-};
+  hover.value = true
+  mousePosition.x = event.clientX
+  mousePosition.y = event.clientY
+}
+
+
+const handleDragRabbit = (rabbitParam) => {
+  //set DRAGGING to whatever dragging isnt
+  dragging.value = !dragging.value
+  const matchingRabbit = rabbitArray.value.find((rabbit) => rabbit.id === rabbitParam.id)
+  //set rabbit.dragged to true, so we can then set the rabbits fall speed inside component
+  matchingRabbit.dragged = true
+}
 </script>
 
 <template>
-    <section class="mousePosition">
-        <h1>
-            Mouse position is at {{ mousePosition.x }},
-            {{ mousePosition.y }}
-        </h1>
+  <section class="mousePosition">
+    <h1>
+      Mouse position is at {{ mousePosition.x }},
+      {{ mousePosition.y }} 
+    </h1>
+  </section>
+
+  <!-- Container for Field and Tools, also ensures Rabbits stay within the "FIELD" -->
+  <div style="display: flex; flex-direction: row">
+    <section
+      class="field"
+      @click="addRabbit"
+      @click.right.prevent="removeRabbit()"
+      @mousemove="handleMouseMove"
+      @mouseleave="hover = false"
+      style="padding: 5px"
+    >
+      <!-- Clickable Area that spawns rabbits -->
     </section>
-    <div style="display: flex; flex-direction: row;">
-        <section class="field" @click="addRabbit" @click.right.prevent="removeRabbit()" @mousemove="handleMouseMove"
-            @mouseleave="hover = false">
-            
-            
-            <!-- "v-bind=rabbit" automatically binds the rabbit name, positionX, positionY o the component, 
-            and works as long as the components properties are the same name as the objects Name, X, and Y values-->
-        </section>
-        <Rabbit v-for="rabbit in rabbitArray" @click.right.prevent="removeRabbit(rabbit?.id)" :key="rabbit.id" v-bind="rabbit" :style="{
-                position: 'absolute',
-                zIndex:1,
-                left: rabbit.positionX - rabbit.imgWidth / 2 + 'px',
-                top: rabbit.positionY - rabbit.imgHeight / 2 + 'px',
-            }" />
-        <section class="toolBar">
-            <h1>Tools</h1>
-            <button @click="clearAllRabbits">Clear All Rabbits</button>
-            
-        </section>
-    </div>
+    <Rabbit
+      v-for="rabbit in rabbitArray"
+      @click.right.prevent="removeRabbit(rabbit?.id)"
+      :key="rabbit.id"
+      v-bind="rabbit"
+      @pointerdown="handleDragRabbit(rabbit)"
+      @pointerup="rabbit.dragged = false"
+      style="position: absolute; z-index: 1"
+    />
+    <!-- "v-bind=rabbit" automatically binds the rabbit name, positionX, positionY, etc to the component, 
+        and works as long as the components properties are the same name as the objects Name, X, and Y values-->
     
+    <section class="toolBar">
+      <h1>Tools</h1>
+      <button @click="clearAllRabbits">Clear All Rabbits</button>
+    </section>
+  </div>
 </template>
 
 <style scoped>
 .field {
-    position:relative;
-    overflow: hidden;
-    left: 0px;
-    bottom: 0px;
-    /* width: 100%;
+  position: relative;
+  overflow: hidden;
+  left: 0px;
+  bottom: 0px;
+  /* width: 100%;
     height: 100vh; */
-    width: 100%;
-    height: 85vh;
-    border: 1px solid red;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    overflow: hidden;
+  width: 100%;
+  height: 85vh;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  overflow: hidden;
 }
 .mousePosition {
-    height: 50px;
-    display: flex; 
-    flex-direction: row; 
-    align-items: center;
-    border: 1px solid green;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid green;
 }
 .toolBar {
-    border: 1px solid black;
-    width: 10%;
+  border: 1px solid black;
+  width: 10%;
 }
 </style>
